bce498dcfd682e9f619e46f1ace14b2066e24d6b
naaman falcon <naamanalejandrofalcon@gmail.com>
2020-10-16 06:53:29 -0300

    limpio proyecto

--- react-redux-coopeuch/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import task from '../reducer/task';
-import thunk from 'redux-thunk';
- 
-export default () => {
-    return createStore(task, applyMiddleware(thunk));
-};
